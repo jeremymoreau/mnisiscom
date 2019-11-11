e3d1190a18dd24afe645daf9a70c47bc26c46f1c
Jeremy Moreau <jeremy.moreau@mail.mcgill.ca>
2019-11-11 12:03:38 -0500

    Update setup.py and changelog.txt

--- setup.py
+++ setup.py
@@ -1,29 +1,43 @@
 from setuptools import setup
 from os import path
 
-# read the contents of README.md file
+# Read the contents of README.md file
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='mnisiscom',
-    version='0.3.4',
-    description='A simple command line tool and GUI for computing subtraction ictal SPECT coregistered to MRI (SISCOM). mnisiscom is exclusively intended for research use!',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    keywords='neuroimaging spect mri siscom epilepsy radiology',
-    url='https://github.com/jeremymoreau/mnisiscom',
-    author='Jeremy Moreau',
-    author_email='jeremy.moreau@mail.mcgill.ca',
-    license='MPL 2.0 and Healthcare Disclaimer',
-    packages=['mnisiscom'],
-    entry_points = {
-        'console_scripts': ['mnisiscom=mnisiscom.command_line:run_siscom',
-                            'mnisiscom_gui=mnisiscom.gui:start_gui'],
-    },
-    include_package_data=True,
-    python_requires='>=3.5, <3.8',
-    install_requires=[
+      version='0.4.0',
+      description='A simple command line tool and GUI for computing subtraction ictal SPECT coregistered to MRI (SISCOM). mnisiscom is exclusively intended for research use!',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      keywords='neuroimaging spect mri siscom epilepsy radiology',
+      url='https://github.com/jeremymoreau/mnisiscom',
+      author='Jeremy Moreau',
+      author_email='jeremy.moreau@mail.mcgill.ca',
+      license='MPL 2.0 and Healthcare Disclaimer',
+      classifiers=[
+          'Intended Audience :: Science/Research',
+              'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+              'Programming Language :: Python',
+              'Programming Language :: JavaScript',
+              'Topic :: Scientific/Engineering :: Medical Science Apps.',
+              'Topic :: Scientific/Engineering',
+              'Operating System :: POSIX :: Linux',
+              'Operating System :: MacOS',
+              'Operating System :: Microsoft :: Windows',
+              'Programming Language :: Python :: 3.5',
+              'Programming Language :: Python :: 3.6',
+              'Programming Language :: Python :: 3.7',
+      ],
+      packages=['mnisiscom'],
+      entry_points={
+          'console_scripts': ['mnisiscom=mnisiscom.command_line:run_siscom',
+                              'mnisiscom_gui=mnisiscom.gui:start_gui'],
+      },
+      include_package_data=True,
+      python_requires='>=3.5, <3.8',
+      install_requires=[
           'numpy',
           'nibabel',
           'nipype',
@@ -35,6 +49,6 @@ setup(name='mnisiscom',
           'click',
           'colorama',
           'eel',
-        #   'pywin32 ; platform_system=="Windows"'
+          'pywin32 ; platform_system=="Windows"'
       ],
-    zip_safe=False)
+      zip_safe=False)
