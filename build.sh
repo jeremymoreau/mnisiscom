e2746ce305a6f7b278e4f1bc0423084dc3b7b69f
Jeremy Moreau <jeremy.moreau@mail.mcgill.ca>
2019-11-15 14:04:30 -0500

    Add icon for Mac .app bundle to build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# mnisiscom GUI Linux build script for pip and PyInstaller
+# mnisiscom GUI Mac/Linux build script for pip and PyInstaller
 # To be run in conda env with minimum dependencies installed
 rm -r dist/*
 python setup.py sdist bdist_wheel
@@ -10,4 +10,4 @@ python -m eel ./mnisiscom/gui.py ./mnisiscom/gui --paths ./mnisiscom/ --onefile 
 --hidden-import="sklearn.metrics.pairwise_fast" --hidden-import="sklearn.utils._cython_blas" \
 --hidden-import="sklearn.neighbors.ball_tree" --hidden-import="sklearn.neighbors.typedefs" \
 --hidden-import="sklearn.neighbors.quad_tree", --hidden-import="sklearn.tree" \
---hidden-import="sklearn.tree._utils"
+--hidden-import="sklearn.tree._utils" --icon ./icons/icon-gen/app.icns
