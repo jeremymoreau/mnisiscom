b6fb4c08378960ac4357830eed82d7ed7e8974b1
Jeremy Moreau <jeremy.moreau@mail.mcgill.ca>
2019-12-14 16:26:17 -0500

    Add icon resolutions

--- build_windows.ps1
+++ build_windows.ps1
@@ -4,19 +4,20 @@
 del dist\*.*
 
 # Build python wheel
+python setup.py clean --all
 python setup.py sdist bdist_wheel
 
 # Re-install mnisiscom in env
 pip install --upgrade --force-reinstall --no-deps (Get-ChildItem .\dist\mnisiscom-*.whl | Select-Object -Expand FullName)
 
-# PyInstaller build
-python -m eel .\mnisiscom\mnisiscom.py .\mnisiscom\gui --paths .\mnisiscom\ --noconsole `
---add-data ".\mnisiscom\MNI152_T1.nii;mnisiscom" --add-data ".\mnisiscom\OpenSans-Regular.ttf;mnisiscom" `
---add-data "$env:CONDA_PREFIX\Lib\site-packages\nilearn\plotting\glass_brain_files;nilearn\plotting\glass_brain_files" `
---hidden-import="sklearn.metrics.pairwise_fast" --hidden-import="sklearn.utils._cython_blas" `
---hidden-import="sklearn.neighbors.ball_tree" --hidden-import="sklearn.neighbors.typedefs" `
---hidden-import="sklearn.neighbors.quad_tree", --hidden-import="sklearn.tree" `
---hidden-import="sklearn.tree._utils" -i .\icons\icon-gen\app.ico
+# # PyInstaller build
+# python -m eel .\mnisiscom\mnisiscom.py .\mnisiscom\gui --paths .\mnisiscom\ --noconsole `
+# --add-data ".\mnisiscom\MNI152_T1.nii;mnisiscom" --add-data ".\mnisiscom\OpenSans-Regular.ttf;mnisiscom" `
+# --add-data "$env:CONDA_PREFIX\Lib\site-packages\nilearn\plotting\glass_brain_files;nilearn\plotting\glass_brain_files" `
+# --hidden-import="sklearn.metrics.pairwise_fast" --hidden-import="sklearn.utils._cython_blas" `
+# --hidden-import="sklearn.neighbors.ball_tree" --hidden-import="sklearn.neighbors.typedefs" `
+# --hidden-import="sklearn.neighbors.quad_tree", --hidden-import="sklearn.tree" `
+# --hidden-import="sklearn.tree._utils" -i .\icons\icon-gen\app.ico
 
-# Build InnoSetup Installer
-iscc .\innosetup.iss
+# # Build InnoSetup Installer
+# iscc .\innosetup.iss
