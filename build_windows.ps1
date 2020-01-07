19da78905db3a3d077753ba738d93f84fee996c5
Jeremy Moreau <jeremy.moreau@mail.mcgill.ca>
2020-01-07 16:34:59 -0500

    Update Windows build script (replace conda with virtualenv)

--- build_windows.ps1
+++ build_windows.ps1
@@ -13,14 +13,23 @@ python setup.py sdist bdist_wheel
 pip install --upgrade --force-reinstall --no-deps (Get-ChildItem .\dist\mnisiscom-*.whl | Select-Object -Expand FullName)
 
 # PyInstaller build
-python -m eel .\mnisiscom\mnisiscom.py .\mnisiscom\gui --paths .\mnisiscom\ --noconsole `
---add-data ".\mnisiscom\MNI152_T1.nii;mnisiscom" --add-data ".\mnisiscom\OpenSans-Regular.ttf;mnisiscom" `
---add-data ".\LICENSE.md;mnisiscom" `
---add-data "$env:CONDA_PREFIX\Lib\site-packages\nilearn\plotting\glass_brain_files;nilearn\plotting\glass_brain_files" `
+pyinstaller .\mnisiscom\mnisiscom.py --paths .\mnisiscom\ `
+--paths C:\Users\jerem\virtualenv\mnisiscom_distv\Lib\site-packages\scipy\.libs `
+--paths C:\Users\jerem\virtualenv\mnisiscom_distv\Lib\site-packages\sklearn\.libs `
+--noconsole  `
+--add-data "$env:VIRTUAL_ENV\Lib\site-packages\eel\eel.js;eel" `
+--add-data ".\mnisiscom\gui;mnisiscom_data\gui" `
+--add-data ".\mnisiscom\MNI152_T1.nii;mnisiscom_data" `
+--add-data ".\mnisiscom\OpenSans-Regular.ttf;mnisiscom_data" `
+--add-data ".\LICENSE.md;mnisiscom_data" `
+--add-data ".\README.md;mnisiscom_data" `
+--add-data "$env:VIRTUAL_ENV\Lib\site-packages\nilearn\plotting\glass_brain_files;nilearn\plotting\glass_brain_files" `
+--hidden-import="bottle_websocket" `
 --hidden-import="sklearn.metrics.pairwise_fast" --hidden-import="sklearn.utils._cython_blas" `
 --hidden-import="sklearn.neighbors.ball_tree" --hidden-import="sklearn.neighbors.typedefs" `
 --hidden-import="sklearn.neighbors.quad_tree", --hidden-import="sklearn.tree" `
---hidden-import="sklearn.tree._utils" -i .\icons\icon-gen\app.ico
+--hidden-import="sklearn.tree._utils" `
+--icon .\icons\icon-gen\app.ico
 
 # Convert LICENSE.md to .rtf for InnoSetup (requires pandoc)
 pandoc -s .\LICENSE.md -o .\innosetup_license.rtf
