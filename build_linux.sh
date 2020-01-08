86ea6c4e4ef4b6be8792c1c904815d0b92f62278
Jeremy Moreau <jeremy.moreau@mail.mcgill.ca>
2020-01-08 17:36:39 -0500

    Change Linux build script to use Ubuntu 18.04 VM for build
    Reduced package size by using a clean Ubuntu install with minimal dependencies installed.

--- build_linux.sh
+++ build_linux.sh
@@ -2,15 +2,15 @@
 # mnisiscom GUI Linux build script for pip and PyInstaller
 # To be run in virtualenv with minimum dependencies installed
 version="0.4.0"
-VIRTUAL_ENV="/export02/data/jeremy/bin/virtualenv/mnisiscom_distv"
+site_packages="/home/jmoreau/.local/lib/python3.6/site-packages"
 
 # Delete all files in dist and build folders
 rm -r dist/*
 rm -r build/*
 
 # Build python wheel
-python setup.py clean --all
-python setup.py sdist bdist_wheel
+python3 setup.py clean --all
+python3 setup.py sdist bdist_wheel
 
 # Re-install mnisiscom in env
 pip install --upgrade --force-reinstall --no-deps dist/mnisiscom-*.whl
@@ -18,13 +18,13 @@ pip install --upgrade --force-reinstall --no-deps dist/mnisiscom-*.whl
 # PyInstaller build
 pyinstaller ./mnisiscom/mnisiscom.py --paths ./mnisiscom/ \
 --onefile --noconsole \
---add-data "$VIRTUAL_ENV/lib/python3.7/site-packages/eel/eel.js:eel" \
+--add-data "$site_packages/eel/eel.js:eel" \
 --add-data "./mnisiscom/gui:mnisiscom_data/gui" \
 --add-data "./mnisiscom/MNI152_T1.nii:mnisiscom_data" \
 --add-data "./mnisiscom/OpenSans-Regular.ttf:mnisiscom_data" \
 --add-data "./LICENSE.md:mnisiscom_data" \
 --add-data "./README.md:mnisiscom_data" \
---add-data "$VIRTUAL_ENV/lib/python3.7/site-packages/nilearn/plotting/glass_brain_files:nilearn/plotting/glass_brain_files" \
+--add-data "$site_packages/nilearn/plotting/glass_brain_files:nilearn/plotting/glass_brain_files" \
 --hidden-import="bottle_websocket" \
 --hidden-import="PIL._imagingft" \
 --hidden-import="sklearn.metrics.pairwise_fast" --hidden-import="sklearn.utils._cython_blas" \
